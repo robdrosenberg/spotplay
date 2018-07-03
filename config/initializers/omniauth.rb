3a86394344dd47e3eb4d6cc51c925e8ae49f87b0
Robert Rosenberg <robertdr85@gmail.com>
2018-07-02 20:47:12 -0700

    Fix search issue and added authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-# require 'rspotify/oauth'
+require 'rspotify/oauth'
 
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :spotify, ENV["CLIENT_ID"], ENV["SECRET_ID"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :spotify, ENV["CLIENT_ID"], ENV["SECRET_ID"], scope: 'playlist-modify-public user-library-modify playlist-modify-private user-modify-playback-state'
+end
